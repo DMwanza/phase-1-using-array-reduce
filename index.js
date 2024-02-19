2b74a487a8fe190d806a5cbfbac18ad53da7c565
Dennis <dennismusembi344@gmail.com>
2024-02-20 01:31:40 +0300

    done2

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-const totalBatteries=batteryBatches.reduce(function(batch,curr){
-    return batch+=curr
-})
+// const totalBatteries=batteryBatches.reduce(function(batch,curr){
+//     return batch+=curr
+// })
 // Code your solution here
+const totalBatteries=batteryBatches.reduce((acc,curr)=>{
+    return acc+=curr
+},0)
